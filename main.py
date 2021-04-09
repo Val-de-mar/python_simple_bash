8fee34a6d007654219f2c4c9d313ff788b3b2dbc
Val-de-mar <vladimir.a.melnikov@phystech.edu>
2021-04-09 15:52:43 +0300

    ls flag -a added

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import os
 import sys
+from executer import Executer
+import executer
 from time import sleep
 import re
 from os import path
+
+
+class FictiveStream:
+    def __init__(self):
+        self.list_str = []
+
+    def write(self, *args):
+        self.list_str += [' '.join(args)]
+
+    def clear(self):
+        self.list_str = []
+
+    def read(self, joint="\n"):
+        return joint.join(self.list_str)
+
+
+demiurge = executer.Executer()
 
 
 class NonDirName(Exception):
@@ -20,116 +39,13 @@ class InvalidArgument(Exception):
 class NonFileName(Exception):
     pass
 
+
 class InvalidDir(Exception):
     pass
 
+
 class CurrentDirDeleting(Exception):
     pass
-
-
-class Executer:
-    def __init__(self):
-        pass
-
-    def chdir(self, subcommand):
-
-        place = path.expanduser(subcommand[0])
-        if os.path.isdir(place):
-            os.chdir(place)
-        else:
-            raise NonDirName
-
-    def listdir(self, aim_path: str):
-        if not path.isdir(aim_path):
-            raise NonDirName
-        aim_path += '/'
-        list_dir = os.listdir(aim_path)
-        ans = []
-        for element in list_dir:
-            if path.isdir(aim_path + element):
-                preans = "\033[34m"
-            elif os.access(aim_path + element, os.X_OK):
-                preans = "\033[32m"
-            else:
-                preans = "\033[37m"
-            ans += [preans + element]
-
-        result = "\n".join(ans)
-        result += "\033[37m"
-        return result
-
-    def copy(self, input: str, output: str):
-        if not path.isdir(path.dirname(output)):
-            if path.dirname(output) != "":
-                raise NonDirName
-        if not path.isfile(input):
-            raise InvalidSyntax
-        if path.isdir(output):
-            output += "/"
-            output += path.basename(input)
-        os.system("cp " + input + " " + output)
-
-    def move(self, input: str, output: str):
-        if not path.isdir(path.dirname(output)):
-            if path.dirname(output) != "":
-                raise NonDirName
-        if not path.isfile(input):
-            raise InvalidSyntax
-        if path.isdir(output):
-            output += "/"
-            output += path.basename(input)
-        os.system("mv " + input + ' ' + output)
-
-    def remove_file(self, filename: str):
-        if not path.isfile(filename):
-            raise NonFileName
-        os.remove(filename)
-
-    def remove_directory(self, directory_name: str):
-        if not path.isdir(directory_name):
-            raise NonDirName
-        if os.listdir(directory_name) != []:
-            raise InvalidArgument
-        os.removedirs(directory_name)
-
-    def make_directory(self, dirname):
-        if not path.isdir(path.dirname(dirname)):
-            raise NonDirName
-
-        os.makedirs(dirname)
-
-    def cat(self, filename):
-        if not path.isfile(filename):
-            raise NonFileName
-        file = open(filename, 'r')
-        ans = file.read()
-        file.close()
-        return ans
-
-    def touch(self, filename):
-        if not path.isdir(path.dirname(filename)):
-            raise NonDirName
-        if path.exists(filename):
-            raise InvalidArgument
-        file = open(filename, 'w')
-        file.close()
-
-
-class FictiveStream:
-    def __init__(self):
-        self.list_str = []
-
-    def write(self, *args):
-        self.list_str += [' '.join(args)]
-
-    def clear(self):
-        self.list_str = []
-
-    def read(self, joint="\n"):
-        return joint.join(self.list_str)
-
-
-demiurge = Executer()
 
 
 class Terminal:
@@ -173,6 +89,13 @@ class Terminal:
         return ans
 
     def listdir(self, subcommand: list):
+        flags = []
+        for part in enumerate(subcommand):
+            if part[1][0] == "-" and len(part[1]) != 1:
+                flags += list(part[1])
+                flags.remove("-")
+                subcommand.pop(part[0])
+
         if not subcommand:
             subcommand.append(self.cur_dir_name)
         elif len(subcommand) != 1 or type(subcommand[0]) is not str:
@@ -181,7 +104,7 @@ class Terminal:
 
         place = self.deprivat(subcommand[0])
         try:
-            self.out_stream.write(demiurge.listdir(place))
+            self.out_stream.write(demiurge.listdir(place, "".join(flags)))
         except NonDirName:
             self.err_stream.write("not directory name")
 
